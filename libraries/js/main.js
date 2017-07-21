9dd4586bb358e7368ff1be6b6460fba35f1fd43d
Junaid Masoodi <junaidmasoodi@outlook.com>
2017-07-21 11:53:03 +0530

    New changes to home

---
+++ libraries/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#owl").owlCarousel({
+        navigation: false,
+        slideSpeed: 100,
+        paginationSpeed: 800,
+        items: 3,
+        autoPlay: false
+    });
+});
