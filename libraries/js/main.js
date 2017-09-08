b6ac87087510e0788f90c1d329ba447449408de1
Junaid Masoodi <junaidmasoodi@outlook.com>
2017-09-08 16:34:49 +0530

    Adding register page.

--- libraries/js/main.js
+++ libraries/js/main.js
@@ -6,4 +6,38 @@ $(document).ready(function() {
         items: 3,
         autoPlay: false
     });
+
+    $(window).load(function() {
+        $(".col-3 input").val("");
+
+        $(".input-effect input").focusout(function() {
+            if ($(this).val() != "") {
+                $(this).addClass("has-content");
+            } else {
+                $(this).removeClass("has-content");
+            }
+        })
+    });
+    $(".sign--up").click(function() {
+        $(this).addClass("enabled");
+        $(".sign").removeClass("enabled");
+        $(".sign").addClass("disabled");
+        $(".sign--in").addClass("remove");
+        $(".register").removeClass("remove");
+        $(".register").addClass("show");
+        $(".sign--in").removeClass("show");
+        $(".sign--get").removeClass("remove");
+        $(".register--get").addClass("remove");
+    });
+    $(".sign").click(function() {
+        $(this).addClass("enabled");
+        $(".sign--up").removeClass("enabled");
+        $(".sign--up").addClass("disabled");
+        $(".register").addClass("remove");
+        $(".register").removeClass("show");
+        $(".sign--in").removeClass("remove");
+        $(".sign--in").addClass("show");
+        $(".register--get").removeClass("remove");
+        $(".sign--get").addClass("remove");
+    });
 });
