0427c1411568804dffc5a2e15e424c7525beef4c
Junaid Masoodi <junaidmasoodi@outlook.com>
2017-11-26 17:16:32 +0530

    destination 2

--- libraries/js/main.js
+++ libraries/js/main.js
@@ -40,4 +40,30 @@ $(document).ready(function() {
         $(".register--get").removeClass("remove");
         $(".sign--get").addClass("remove");
     });
+    (function($) {
+        $.fn.spinner = function() {
+            this.each(function() {
+                var el = $(this);
+
+                // add elements
+                el.wrap('<span class="spinner"></span>');
+                el.before('<span class="sub">-</span>');
+                el.after('<span class="add">+</span>');
+
+                // substract
+                el.parent().on('click', '.sub', function() {
+                    if (el.val() > parseInt(el.attr('min')))
+                        el.val(function(i, oldval) { return --oldval; });
+                });
+
+                // increment
+                el.parent().on('click', '.add', function() {
+                    if (el.val() < parseInt(el.attr('max')))
+                        el.val(function(i, oldval) { return ++oldval; });
+                });
+            });
+        };
+    })(jQuery);
+
+    $('input[type=number]').spinner();
 });
